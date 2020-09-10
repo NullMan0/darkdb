159b09098e1fc87b2026986332312c282ff3d1e1
NullMan <71032126+NullMan0@users.noreply.github.com>
2020-09-10 10:28:41 +0300

    Update index.js
    Kekoyaz & Sifre Fonksiyonu Eklendi

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const request = require('node-superfetch');
 
 module.exports = {
     sifre: async function(yazi) {
-        if(!yazi) throw new TypeError('Lütfen bir dizi ismi gir. Örneğin; Lucifer')
+        if(!yazi) throw new TypeError('Lütfen bir sayı gir.')
         if(isNaN(yazi)) throw new TypeError('Bir Sayı Olması Lazım')
            try {
-            const { body } = await request.get("https://www.nullman.tk/api/sifre?sifre="+yazi.toLowerCase());
+            const { body } = await request.get("https://www.nullman.tk/api/sifre/"+yazi.toLowerCase());
+            return body
+        } catch (err) {
+            throw new TypeError(err);
+        }
+    },
+    kekoyaz: async function(yazi) {
+        if(!yazi) throw new TypeError('Lütfen bir yazı gir.')
+           try {
+            const { body } = await request.get("https://www.nullman.tk/api/kekoyaz/"+yazi.toLowerCase());
             return body
         } catch (err) {
             throw new TypeError(err);
