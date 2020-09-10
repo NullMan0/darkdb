9862a0d1cadb79caeeb7b276556a005ba59586e1
NullMan <71032126+NullMan0@users.noreply.github.com>
2020-09-10 10:18:32 +0300

    Update index.js
    sifre fonksiyonu eklendj

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const lowdb = require("lowdb")
- 
-exports.run()
+const request = require('node-superfetch');
+
+module.exports = {
+    sifre: async function(yazi) {
+        if(!yazi) throw new TypeError('Lütfen bir dizi ismi gir. Örneğin; Lucifer')
+        if(isNaN(yazi)) throw new TypeError('Bir Sayı Olması Lazım')
+           try {
+            const { body } = await request.get("https://www.nullman.tk/api/sifre?sifre="+yazi.toLowerCase());
+            return body
+        } catch (err) {
+            throw new TypeError(err);
+        }
+    }
+ }
