e69e88cc47959ff0975f36597cdac17713c5aada
NullMan <71032126+NullMan0@users.noreply.github.com>
2020-09-12 12:25:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const request = require('node-superfetch');
+const request = require('axios');
 
 module.exports = {
     sifre: async function(yazi) {
@@ -34,4 +34,51 @@ module.exports = {
             throw new TypeError(err);
         }
     }   
+ }const request = require('axios')
+/*
+
+© NullAPI ©
+
+*/
+module.exports = {
+    sifre: async function(yazi) {
+        if(!yazi) throw new TypeError('Lütfen bir sayı gir.')
+        if(isNaN(yazi)) throw new TypeError('Bir Sayı Olması Lazım')
+           try {
+            const { body } = await request.get("https://www.nullman.tk/api/sifre/"+yazi.toLowerCase());
+            return body
+        } catch (err) {
+            throw new TypeError(err);
+        }
+    },
+  /*
+
+© NullAPI ©
+
+*/
+    kekoyaz: async function(yazi) {
+        if(!yazi) throw new TypeError('Lütfen bir yazı gir.')
+           try {
+            const { body } = await request.get("https://www.nullman.tk/api/kekoyaz/"+yazi.toLowerCase());
+            return body
+        } catch (err) {
+            throw new TypeError(err);
+        }
+    },
+ /*
+
+© NullAPI ©
+
+*/
+    tdk: async function(yazi) {
+        if(!yazi) throw new TypeError('Lütfen bir kelime gir.')
+ 
+const body = await request.get("https://sozluk.gov.tr/gts?ara="+yazi)
+if(body.data.error) {
+  return false
+}else{
+  return true
+}
+ 
+    }
  }
