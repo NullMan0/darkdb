fb1447256ed016b2ad9948f86cdbc813661650f7
NullMan <71032126+NullMan0@users.noreply.github.com>
2020-09-12 12:33:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,4 @@
-const request = require('axios');
-
-module.exports = {
-    sifre: async function(yazi) {
-        if(!yazi) throw new TypeError('Lütfen bir sayı gir.')
-        if(isNaN(yazi)) throw new TypeError('Bir Sayı Olması Lazım')
-           try {
-            const { body } = await request.get("https://www.nullman.tk/api/sifre/"+yazi.toLowerCase());
-            return body
-        } catch (err) {
-            throw new TypeError(err);
-        }
-    },
-    kekoyaz: async function(yazi) {
-        if(!yazi) throw new TypeError('Lütfen bir yazı gir.')
-           try {
-            const { body } = await request.get("https://www.nullman.tk/api/kekoyaz/"+yazi.toLowerCase());
-            return body
-        } catch (err) {
-            throw new TypeError(err);
-        }
-    },
- ,
-    tdk: async function(yazi) {
-        if(!yazi) throw new TypeError('Lütfen bir kelime gir.')
-           try {
-            const { body } = await request.get("https://sozluk.gov.tr/gts?ara="+yazi.toLowerCase());
-            if(body === "error") {
-                return true
-                }else{
-                    return false
-                    }
-        } catch (err) {
-            throw new TypeError(err);
-        }
-    }   
- }const request = require('axios')
+const request = require('axios')
 /*
 
 © NullAPI ©
@@ -45,8 +9,8 @@ module.exports = {
         if(!yazi) throw new TypeError('Lütfen bir sayı gir.')
         if(isNaN(yazi)) throw new TypeError('Bir Sayı Olması Lazım')
            try {
-            const { body } = await request.get("https://www.nullman.tk/api/sifre/"+yazi.toLowerCase());
-            return body
+            const body = await request.get("https://www.nullman.tk/api/sifre/"+yazi.toLowerCase());
+            return body.data.sifre
         } catch (err) {
             throw new TypeError(err);
         }
@@ -56,11 +20,11 @@ module.exports = {
 © NullAPI ©
 
 */
-    kekoyaz: async function(yazi) {
+    kekoyazi: async function(yazi) {
         if(!yazi) throw new TypeError('Lütfen bir yazı gir.')
            try {
-            const { body } = await request.get("https://www.nullman.tk/api/kekoyaz/"+yazi.toLowerCase());
-            return body
+            const body = await request.get("https://www.nullman.tk/api/kekoyaz/"+yazi.toLowerCase());
+            return body.data.yazi
         } catch (err) {
             throw new TypeError(err);
         }
